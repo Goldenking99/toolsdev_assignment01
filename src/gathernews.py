2db623246418c83c061ab681c5b8a1b3e3e790e4
Goldenking99 <66437587+Goldenking99@users.noreply.github.com>
2020-06-12 03:48:16 -0500

    Updated gathernews.py
    Debugged code and ran some test runs to ensure it worked properly, still some hiccups on opening into web browser

--- src/gathernews.py
+++ src/gathernews.py
@@ -3,12 +3,12 @@
 #Import the libraries and build necessary variables
 import nltk
 import newspaper
-from newspaper import articles
+import webbrowser
 
 #Get the news sources
-cnn_paper = newspaper.build('http://cnn.com')
-fox_paper = newspaper.build('http://foxnews.com')
-ind_paper = newspaper.build('http://independent.co.uk/us')
+cnn_paper = 'http://cnn.com'
+fox_paper = 'http://foxnews.com'
+ind_paper = 'http://independent.co.uk/us'
 
 newsSrc = (cnn_paper, fox_paper, ind_paper)
 collected_articles = list()
@@ -17,22 +17,22 @@ keyworded_articles = list()
 save_txt = 'news_summary.txt'
 
 #Functions to aid scraping
-def crawl(source)
-    source_paper = newspaper.build(source, memoize_articles.False)
+def crawl(source):
+    source_paper = newspaper.build(source, memoize_articles=False)
     print("scrape size: " + str(source_paper.size()))
     for article in source_paper.articles:
             collected_articles.append(article)
 
-def crawl_list(sources)
+def crawl_list(sources):
     newspapers = list()
     for source in sources:
         crawl(source)
 
 #Program asks user for a keyword and implements the keyword
 crawl_list(newsSrc)
-keyWord = raw_input("Please enter any keyword(press enter to continue) ")
+keyWord = input("Please enter any keyword(press enter to continue) ")
 
-for articles in collected_articles:
+for article in collected_articles:
 	    if keyWord in article.url:
 	    	    keyworded_articles.append(article)
 
@@ -54,8 +54,8 @@ for article in keyworded_articles:
 write.write('\n' + str(article.title))
 write.write(' - ')
 
-for author in article.authors
-	    if author != article.authors[0]
+for author in article.authors:
+	    if author != article.authors[0]:
 	    	    writer.write(', ')
 
 	    write.write(str(author))
@@ -68,7 +68,7 @@ write.close()
 #Open articles on web
 web_open = input('Open all ' + str(len(keyworded_articles)) + ' on web?\n [Y] Yes; [N] No; ')
 
-	web.open.lower() in ('y'):
-	    webbrowser.opeb(collected_articles[0].url, new=1, autoraise=True)
-	    for num in range(1, len(keyworded_articles)-1):
-	    	    webbrowser.open(collected_articles[num].url, new=2)
+if web_open.lower() in ('y'):
+    webbrowser.open(collected_articles[0].url, new=1, autoraise=True)
+    for num in range(1, len(keyworded_articles)-1):
+        webbrowser.open(collected_articles[num].url, new=2)
